9795fd1e290d3a7ee71cb62b4afb719fc26b8b51
Akbardanisaputra <danissaputra909@gmail.com>
2025-02-19 17:02:20 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -2,18 +2,18 @@
 
 require_once("./Telebot.php");
 
-// initialize bot
-$bot = new Telebot(8138496370:AAFxwCWctQnMn9QyaM9VT4HSuCjae5Z5Tnc);
+// Masukkan token bot sebagai string
+$bot = new Telebot("8138496370:AAFxwCWctQnMn9QyaM9VT4HSuCjae5Z5Tnc");
 
-// handle start command
+// Handle start command
 $bot->command("start", function ($ctx) {
     $ctx->replyWithText("Kamu mengirimkan command /start");
 });
 
-// handle hello command
+// Handle hello command
 $bot->command("hello", function ($ctx) {
-    $ctx->replyWithText("Halo kak " . $ctx->from->first_name);
+    $ctx->replyWithText("Halo kak " . $ctx->from["first_name"]);
 });
 
-// run bot
+// Jalankan bot
 $bot->run();
